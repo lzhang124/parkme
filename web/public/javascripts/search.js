99bf189e4401b3b0ee866bf461dc4c59527f5c7d
Larry Zhang <lzhang124@gmail.com>
2015-06-06 18:41:34 -0600

    basic zipcode search

--- web/public/javascripts/search.js
+++ web/public/javascripts/search.js
@@ -1,9 +1,14 @@
 var app = angular.module('search', []);
 
 app.controller('searchController', function($scope, $http) {
+  $scope.showSearch = true;
 
   $scope.showResults = function() {
     var search = this.search;
-    $http.get('https://127.0.0.1:8443/lotsByZipcode?zipcode='+search).success(function(){console.log('hello')})
+    $http.get('https://127.0.0.1:8443/lotsByZipcode?zipcode='+search)
+      .success(function(response){
+        $scope.lots = response;
+      });
+    $scope.showSearch = false;
   };
 });
