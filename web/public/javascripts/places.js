ad2edaa3f2dc91f9d41e8019c69a5d7154e297d9
Larry Zhang <lzhang124@gmail.com>
2015-06-15 23:37:55 -0600

    fixed scaling

--- web/public/javascripts/places.js
+++ web/public/javascripts/places.js
@@ -6,56 +6,46 @@ function initialize() {
   var search = document.getElementById('search');
   var auto_search = new google.maps.places.Autocomplete(search);
 
+  map = new google.maps.Map(document.getElementById('map'), {
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    zoom: 18
+  });
+
   google.maps.event.addListener(auto_search, 'place_changed', function() {
     var start = auto_search.getPlace().geometry.location;
-    
-    if (map) {
-      map.panTo(start);
-      map.setZoom(17);
+    map.panTo(start);
+    map.setZoom(18);
 
-      var request = {
-        location: start,
-        radius: 400,
-        types: ['parking']
-      };
+    var request = {
+      location: start,
+      radius: 400,
+      types: ['parking']
+    };
 
-      infowindow = new google.maps.InfoWindow();
-      var service = new google.maps.places.PlacesService(map);
-      service.nearbySearch(request, callback);
-    } else {
-      map = new google.maps.Map(document.getElementById('map'), {
-        mapTypeId: google.maps.MapTypeId.ROADMAP,
-        center: start,
-        zoom: 17
-      });
-
-      var request = {
-        location: start,
-        radius: 400,
-        types: ['parking']
-      };
-
-      infowindow = new google.maps.InfoWindow();
-      var service = new google.maps.places.PlacesService(map);
-      service.nearbySearch(request, callback);
-    }
+    infowindow = new google.maps.InfoWindow();
+    var service = new google.maps.places.PlacesService(map);
+    service.nearbySearch(request, callback);
   });
 }
 
 function callback(results, status) {
   if (status == google.maps.places.PlacesServiceStatus.OK) {
+    var bounds = map.getBounds();
     for (var i = 0; i < results.length; i++) {
-      createMarker(results[i]);
+      createMarker(results[i], bounds);
     }
+    map.fitBounds(bounds);
   }
 }
 
-function createMarker(place) {
+function createMarker(place, bounds) {
   var placeLoc = place.geometry.location;
   var marker = new google.maps.Marker({
     map: map,
     position: placeLoc
   });
+
+  bounds.extend(place.geometry.location);
 
   google.maps.event.addListener(marker, 'click', function() {
     infowindow.setContent(place.name);
