792120b57fe264ccd408f0d35a3e4dab7c08ff32
Larry Zhang <lzhang124@gmail.com>
2015-06-18 22:12:27 -0600

    buttons of search results

--- web/public/javascripts/results.js
+++ web/public/javascripts/results.js
@@ -1,37 +1,34 @@
 var app = angular.module('results', []);
 var url = 'https://127.0.0.1:8443/';
 
-app.service('Map', function($http) {
-
+app.controller('resultsController', function($scope, $http) {
   var map;
   var infowindow;
   var markers = [];
 
-  this.init = function() {
-    var search = document.getElementById('search');
-    var auto_search = new google.maps.places.Autocomplete(search);
+  var search = document.getElementById('search');
+  var auto_search = new google.maps.places.Autocomplete(search);
 
-    map = new google.maps.Map(document.getElementById('map'), {
-      mapTypeId: google.maps.MapTypeId.ROADMAP,
-      disableDefaultUI: true
-    });
+  map = new google.maps.Map(document.getElementById('map'), {
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    disableDefaultUI: true
+  });
 
-    google.maps.event.addListener(auto_search, 'place_changed', function() {
-      deleteMarkers();
+  google.maps.event.addListener(auto_search, 'place_changed', function() {
+    deleteMarkers();
 
-      var start = auto_search.getPlace();
-      var startLoc = start.geometry.location;
+    var start = auto_search.getPlace();
+    var startLoc = start.geometry.location;
 
-      map.panTo(startLoc);
-      map.setZoom(17);
+    map.panTo(startLoc);
+    map.setZoom(17);
 
-      createMarker(startLoc.lat(), startLoc.lng(), start.name, null);
-      searchNear(startLoc);
+    createMarker(startLoc.lat(), startLoc.lng(), start.name, null);
+    searchNear(startLoc);
 
-      infowindow = new google.maps.InfoWindow();
-      places = new google.maps.places.PlacesService(map);
-    });
-  }
+    infowindow = new google.maps.InfoWindow();
+    places = new google.maps.places.PlacesService(map);
+  });
 
   var createMarker = function(lat, lng, name, available) {
 
@@ -68,14 +65,11 @@ app.service('Map', function($http) {
     $http.get(url + 'searchNear?longitude=' + location.lng() + 
                                '&latitude=' + location.lat()).
     success(function(response) {
+      $scope.lots = response;
       for (var i = 0; i < response.length; i++) {
         place = response[i];
         createMarker(place.location[1], place.location[0], place.name, place.available);
       }
     })
   }
-});
-
-app.controller('resultsController', function(Map) {
-  Map.init();
 });
