d6a4dab1e6e740c42d0687a4ac9681f148f1e06b
Larry Zhang <lzhang124@gmail.com>
2015-06-18 23:05:09 -0600

    remove zipcode search

--- web/public/javascripts/main.js
+++ web/public/javascripts/main.js
@@ -1,29 +1,6 @@
 var app = angular.module('main', []);
 var url = 'https://127.0.0.1:8443/';
 
-app.controller('searchController', function($scope, $http) {
-  $scope.showSearch = true;
-
-  $scope.showResults = function() {
-    var search = this.search;
-    $http.get(url + 'listLots').
-    success(function(response) {
-      $scope.lots = response;
-    });
-    $scope.showSearch = false;
-  }
-
-  $scope.hideResults = function() {
-    $scope.showSearch = true;
-  }
-
-  $scope.close = function(event) {
-    if (event.keyCode === 27) {
-      $scope.hideResults();
-    }
-  }
-});
-
 app.controller('loginController', function($scope, $http) {
   $scope.showDimmer = false;
   $scope.LoginForm = false;
