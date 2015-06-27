74e81e3d00b0ed2fb57e9022497474986ecd2e83
Larry Zhang <lzhang124@gmail.com>
2015-06-27 16:56:15 -0600

    added calendar

--- web/public/javascripts/register.js
+++ web/public/javascripts/register.js
@@ -1,7 +1,9 @@
 var app = angular.module('register', []);
 var url = 'https://127.0.0.1:8443/';
 
-app.controller('registerController', function($scope, $filter) {
+app.controller('registerController', function($scope, $document, $element) {
+  
+  // INIT MAP //
   var search = document.getElementById('search');
   var auto_search = new google.maps.places.Autocomplete(search);
 
@@ -40,6 +42,8 @@ app.controller('registerController', function($scope, $filter) {
   );
   map.fitBounds(USbounds);
 
+
+  // MAPS SEARCH //
   google.maps.event.addListener(auto_search, 'place_changed', function() {
     var start = auto_search.getPlace();
     var startLoc = start.geometry.location;
@@ -73,6 +77,8 @@ app.controller('registerController', function($scope, $filter) {
     markers = [];
   }
 
+
+  // CAPACITY FIELD //
   $scope.capacity = 1;
   $scope.plusCapacity = function() {
     if ($scope.capacity > 0) {
@@ -95,6 +101,8 @@ app.controller('registerController', function($scope, $filter) {
     }
   }
 
+
+  // RESERVABLE FIELD //
   $scope.reservable = 1;
   $scope.plusReserve = function() {
     if ($scope.reservable >= 0) {
@@ -114,5 +122,138 @@ app.controller('registerController', function($scope, $filter) {
     }
   }
 
-  $scope.type = "hour";
+  // CALENDAR DATA //
+  $scope.week = [
+    'Sun',
+    'Mon',
+    'Tue',
+    'Wed',
+    'Thu',
+    'Fri',
+    'Sat'
+  ];
+  $scope.times = [
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    '10',
+    '11',
+    '12',
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    '10',
+    '11'
+  ];
+
+
+  // INIT CALENDAR //
+  var hours = 24;
+  var days = 7;
+  $scope.schedule = new Array(days);
+  for (var i = 0; i < days; i++) {
+    $scope.schedule[i] = new Array(hours);
+  }
+
+
+  // DRAG TO SELECT //
+  var startCell = null;
+  var dragging = false;
+
+  function mouseUp(el) {
+    startCell = null;
+    dragging = false;
+  }
+  
+  function mouseDown(el) {
+    dragging = true;
+
+    var cell = getCoords(el);
+    if ($scope.schedule[cell.day][cell.hour] === 1) {
+      $scope.schedule[cell.day][cell.hour] = null;
+    } else {
+      $scope.schedule[cell.day][cell.hour] = 1;
+    }
+    startCell = cell;
+  }
+
+  function mouseEnter(el) {
+    if (!dragging) return;
+    
+    var cell = getCoords(el);
+    if ($scope.schedule[startCell.day][startCell.hour] === 1) {
+      $scope.schedule[cell.day][cell.hour] = 1;
+    } else {
+      $scope.schedule[cell.day][cell.hour] = null;
+    }
+  }
+        
+  function getCoords(cell) {
+    var column = cell[0].cellIndex;
+    var row = cell.parent()[0].rowIndex;
+    return {
+      day: column,
+      hour: row
+    };
+  }
+  
+  function wrap(fn) {
+    return function() {
+      var el = angular.element(this);
+      $scope.$apply(function() {
+        fn(el);
+      });
+    }
+  }
+  
+  $element.delegate('td', 'mousedown', wrap(mouseDown));
+  $element.delegate('td', 'mouseenter', wrap(mouseEnter));
+  $document.delegate('body', 'mouseup', wrap(mouseUp));
+
+
+  // CALCULATE TIMES //
+  $scope.scheduleTimes = function() {
+    var offset = new Date().getTimezoneOffset()*60*1000;
+    var date = Math.floor(new Date()/604800000)*604800000 - 345600000 + offset;
+
+    $scope.startTimes = [];
+    $scope.durations = [];
+    var block = false;
+    var duration;
+    for (var day = 0; day < 7; day++) {
+      for (var hour = 0; hour < 24; hour++) {
+        if ($scope.schedule[day][hour] === 1) {
+          if (block) {
+            duration++;
+          } else {
+            $scope.startTimes.push(date + day*86400000 + hour*3600000);
+            console.log(new Date(date + day*86400000 + hour*3600000));
+            block = true;
+            duration = 1;
+          }
+        } else {
+          if (block) {
+            $scope.durations.push(duration);
+            block = false;
+          }
+        }
+      }
+      if (block) {
+        $scope.durations.push(duration);
+        block = false;
+      }
+    }
+  }
 });
