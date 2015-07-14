f5cf54848366998ba3e0eccd9a0319eb2bfea1d5
Larry Zhang <lzhang124@gmail.com>
2015-07-14 14:27:08 -0600

    add parth page

--- web/routes/index.js
+++ web/routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var http = require('http');
 
 var isAuthenticated = function (req, res, next) {
   if (req.isAuthenticated()) {
@@ -13,9 +14,23 @@ router.get('/', function(req, res) {
   res.render('splash');
 });
 
+/* GET parth's page. */
+router.get('/DJYWcyUqGV', function(req, res) {
+  http.get('http://52.25.5.25:8080/lotById?lotId=559a2799e4b07b965fbdf711', function(response) {
+      var body = '';
+      response.on('data', function(d) {
+        body += d;
+      });
+      response.on('end', function() {
+        var data = JSON.parse(body);
+        res.render('parth', { lot : data });
+      });
+  });
+});
+
 /* GET home page. */
 router.get('/q9xwGoXLGQ', function(req, res) {
-  res.render('index', { user: req.user });
+  res.render('index');
 });
 
 /* GET about page. */
@@ -25,12 +40,12 @@ router.get('/q9xwGoXLGQ/about', function(req, res) {
 
 /* GET profile page. */
 router.get('/q9xwGoXLGQ/profile', isAuthenticated, function(req, res) {
-  res.render('profile', { user: req.user });
+  res.render('profile');
 });
 
 /* GET register page. */
 router.get('/q9xwGoXLGQ/register', isAuthenticated, function(req, res) {
-  res.render('register', { user: req.user });
+  res.render('register');
 });
 
 module.exports = router;
