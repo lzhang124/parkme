4b2ff3f36e9e6c17749602f87e2c8918cc7291e6
Larry Zhang <lzhang124@gmail.com>
2015-07-16 16:46:43 -0600

    disable button without login

--- web/routes/api.js
+++ web/routes/api.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var LotController = require('../controllers/lot-controller');
 var SearchController = require('../controllers/search-controller');
+var ReservationController = require('../controllers/reservation-controller');
 
 var isAuthenticated = function (req, res, next) {
   if (req.isAuthenticated()) {
--- web/routes/index.js
+++ web/routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var http = require('http');
+var ReservationController = require('../controllers/reservation-controller');
 
 var isAuthenticated = function (req, res, next) {
   if (req.isAuthenticated()) {
@@ -17,14 +18,14 @@ router.get('/', function(req, res) {
 /* GET parth's page. */
 router.get('/DJYWcyUqGV', function(req, res) {
   http.get('http://52.25.5.25:8080/lotById?lotId=55a58121e4b081c25a7efae0', function(response) {
-      var body = '';
-      response.on('data', function(d) {
-        body += d;
-      });
-      response.on('end', function() {
-        var data = JSON.parse(body);
-        res.render('parth', { lot : data });
-      });
+    var body = '';
+    response.on('data', function(d) {
+      body += d;
+    });
+    response.on('end', function() {
+      var data = JSON.parse(body);
+      res.render('parth', { lot : data });
+    });
   });
 });
 
@@ -47,5 +48,8 @@ router.get('/q9xwGoXLGQ/profile', isAuthenticated, function(req, res) {
 router.get('/q9xwGoXLGQ/register', isAuthenticated, function(req, res) {
   res.render('register');
 });
+
+/* GET reservations page. */
+router.get('/q9xwGoXLGQ/reservations/:lotId', isAuthenticated, ReservationController.getReservations);
 
 module.exports = router;
